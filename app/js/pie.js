cb2b0e8694a65e73c719d9bc09fb98b28da26d4b
Dave Gumba <dave_gumba@Yahoo.com>
2017-01-21 21:48:26 -0500

    Trying to extract csv data.

---
+++ app/js/pie.js
@@ -0,0 +1,47 @@
+(function(d3) {
+  'use strict';
+
+  d3.csv('data/DriversInAccidents.csv', function(data) {
+    data.forEach(function(d) {
+      console.log(d[0]);
+    });
+  });
+
+
+  var dataset = [
+    { label: 'Male', count: 10 },
+    { label: 'Female', count: 20 },
+    { label: 'Null', count: 3 }
+  ];
+
+  var width = 300;
+  var height = 400;
+  var radius = Math.min(width, height) / 2;
+
+  var color = d3.scaleOrdinal()
+    .range(['#00A7E1', '#007EA7', '#003459']);
+
+  var svg = d3.select('#gender-pie-chart')
+    .append('svg')
+    .attr('width', width)
+    .attr('height', height)
+    .append('g')
+    .attr('transform', 'translate(' + (width / 2) + ',' + (height / 2) + ')');
+
+  var arc = d3.arc()
+    .innerRadius(0)
+    .outerRadius(radius);
+
+  var pie = d3.pie()
+    .value(function(d) { return d.count; })
+    .sort(null);
+
+  var path = svg.selectAll('path')
+    .data(pie(dataset))
+    .enter()
+    .append('path')
+    .attr('d', arc)
+    .attr('fill', function(d) {
+      return color(d.data.label);
+    });
+  })(window.d3);
